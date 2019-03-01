7c302e6a0fe553f0b3b97ca987b88f9d1d9a274e
Joey Gauthier <digitaljedi@outlook.com>
2019-03-01 15:09:33 -0600

    added some images for a blog I started writing and wrote some content that isn't yet finished

--- content/directory/gaming.js
+++ content/directory/gaming.js
@@ -12,10 +12,25 @@ export default () => [
       '/images/early_2019_collection.jpg'
     ],
     meta: {
-      id: 4,
+      id: 5,
       keywords:
         'gaming, nes, retro games, genesis, dreamcast, retro game collecting, vintage video games',
       content: 'How I Got Into Video Game Collecting'
+    }
+  },
+  {
+    id: 6,
+    slug: 'the-struggle-of-hookinh-it-all-up',
+    title: `Connecting All of Those Consoles`,
+    created_at: '03/01/2019',
+    intro:
+      `An overview of how I keep all of my consoles connected to my 4K TV for the best picture possible on a budget.`,
+    image: ['/images/cable_mess.jpg'],
+    meta: {
+      id: 6,
+      keywords:
+        'gaming, nes, retro games, genesis, dreamcast, retro game collecting, vintage video games, upscaler, switcher, SCART, component cable, RGB',
+      content: 'Connecting All of Those Consoles'
     }
   }
 ];
