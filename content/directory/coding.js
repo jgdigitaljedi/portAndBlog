86234e1b6377e6536eacff2dc6ab0ede76ff3103
Joey Gauthier <digitaljedi@outlook.com>
2019-05-13 07:15:00 -0500

    added more multiplayer games to blog post, added new paragraph to linux blog post

--- content/directory/coding.js
+++ content/directory/coding.js
@@ -1,4 +1,3 @@
 import * as coding2019 from './coding/2019';
 
 export default () => [].concat.apply([], [coding2019.first, coding2019.second]);
-// will eventually be something like [].concat.apply([], [coding2019.first, coding2019.second, coding2019.third]);
