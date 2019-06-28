fd6bd5dc7f5bde4498dddaa274400d83af5638b5
Joey Gauthier <digitaljedi@outlook.com>
2019-06-28 07:53:10 -0500

    trying to configure apollo vue stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -148,6 +148,7 @@ module.exports = {
     'nuxt-webfontloader',
     '@nuxtjs/dotenv',
     '@nuxtjs/feed',
+    '@nuxtjs/apollo',
     'cookie-universal-nuxt',
     [
       'nuxt-imagemin',
@@ -214,6 +215,54 @@ module.exports = {
         }
       }
     ]
+  },
+
+  /** Apollo config for no-ssr vg collection view */
+  apollo: {
+    tokenName: 'apollo-token', // optional, default: apollo-token
+    tokenExpires: 7, // optional, default: 7 (days)
+    includeNodeModules: false, // optional, default: false (this includes graphql-tag for node_modules folder)
+    authenticationType: 'Bearer', // optional, default: 'Bearer'
+    // (Optional) Default 'apollo' definition
+    defaultOptions: {
+      // See 'apollo' definition
+      // For example: default query options
+      $query: {
+        loadingKey: 'loading',
+        fetchPolicy: 'cache-and-network',
+      },
+    },
+    // optional
+    errorHandler: '~/plugins/apollo-error-handler.js',
+    // required
+    clientConfigs: {
+      default: {
+        // required  
+        httpEndpoint: 'http://localhost:4000',
+        // optional
+        // See https://www.apollographql.com/docs/link/links/http.html#options
+        httpLinkOptions: {
+          credentials: 'same-origin'
+        },
+        // You can use `wss` for secure connection (recommended in production)
+        // Use `null` to disable subscriptions
+        wsEndpoint: 'ws://localhost:4000', // optional
+        // LocalStorage token
+        tokenName: 'apollo-token', // optional
+        // Enable Automatic Query persisting with Apollo Engine
+        persisting: false, // Optional
+        // Use websockets for everything (no HTTP)
+        // You need to pass a `wsEndpoint` for this to work
+        websocketsOnly: false // Optional
+      },
+      test: {
+        httpEndpoint: 'http://localhost:5000',
+        wsEndpoint: 'ws://localhost:5000',
+        tokenName: 'apollo-token'
+      },
+      // alternative: user path to config which returns exact same config options
+      test2: '~/plugins/my-alternative-apollo-config.js'
+    }
   },
 
   /** Sitemap generation */
