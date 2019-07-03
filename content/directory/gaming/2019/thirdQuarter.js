6df17416fbe615377b0665d373f9652ffa324082
Joey Gauthier <digitaljedi@outlook.com>
2019-07-03 07:17:46 -0500

    finishing up newest game post

--- content/directory/gaming/2019/thirdQuarter.js
+++ content/directory/gaming/2019/thirdQuarter.js
@@ -4,22 +4,23 @@ export const third = [
     slug: 'dear-video-game-jounalists-keep-your-politics-away-from-my-video-games',
     title: 'Dear Video Game Jounalists: Keep Your Politics Away From My Video Games',
     tag_line: `I've had enough of hack "jounalists" attacking video games. It needs to stop! NOW!`,
-    created_at: '05/31/2019',
+    created_at: '07/03/2019',
     pinned: false,
     intro: `The issues I have with modern video game "journalists" injecting their beliefs and politics into their work.`,
     image:
-      'https://res.cloudinary.com/https-joeyg-me/image/upload/v1559045644/gaming/YouTube-Logo_1.png',
-    alt: `the YouTube logo`,
+      'https://res.cloudinary.com/https-joeyg-me/image/upload/v1562155970/gaming/triggered.png',
+    alt: `image of an angry person with the word tiggered under it`,
     meta: {
       keywords:
-        'video games,retro,Joey Gauthier,video game reviews',
-      hashtags: 'retroGaming, retroGames, YouTube',
+        'video games,retro,Joey Gauthier,video game reviews,sjw,cyberpunk 2077,mortal kombat 11,politics,gender politics',
+      hashtags: 'videoGames',
       description: `My complaints about terrible coverage of video games by popular journalists who refuse to keep their own beliefs out of their reporting.`
     },
     headers: [
       `Let's Set the Stage First`,
       `Gaming Is Supposed to be An Escape`,
-      `Recent Examples of Terrible Video Game "Journalism"`
+      `Recent Examples of Terrible Video Game "Journalism"`,
+      `What Can We Do About These Politically Charged Reviews?`
     ]
   }
 ];
